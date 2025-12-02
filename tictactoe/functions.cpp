c6f6f3590039fa916e4b642fa417e02c903be6a6
Work <Work@MSI>
2025-12-02 11:24:06 -0600

    Did some stuff.

--- tictactoe/functions.cpp
+++ tictactoe/functions.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <map>
 
 
-std::set<std::pair<int, int>> playedPositions; //bad boy (no global vars)
 
 
-void displayBoard(char gameboard[4][4])
+void displayBoard(const std::vector<std::vector<char>>& gameboard)
 {
+	//gameboard[0][0] = 's'; //ain't gonna happen, bro!
+
 	for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
@@ -20,143 +22,53 @@ void displayBoard(char gameboard[4][4])
 	}
 }
 
-void player1Turn(char gameboard[4][4])
+std::pair<int, int> convertStringInputToRowAndColumn(const std::string& input)
 {
-	bool turn = true;
-	std::string input{};
-	std::getline(std::cin, input);
-	while (turn)
+	std::map<std::string, std::pair<int, int>> stringsToRowsAndColumns =
 	{
-		if (input == "a1")
-		{
-			if (playedPositions.find({ 1, 1 }) == playedPositions.end())
-			{
-				gameboard[1][1] = 'O';
-				playedPositions.insert({ 1, 1 });
-				turn = false;
-			}
+		{"a1", {1,1}},
+		{"a2", {2,1}},
+		{"a3", {3, 1}},
 
-			else
-			{
-				std::cout << "That spot is already taken\n";
-				std::getline(std::cin, input);
-			}
-		}
+		// Column 2 (b)
+		{"b1", {1, 2}}, // Row 1, Column 2
+		{"b2", {2, 2}}, // Row 2, Column 2
+		{"b3", {3, 2}}, // Row 3, Column 2
 
-		else if (input == "a2")
-		{
-			gameboard[2][1] = 'O';
-			turn = false;
-		}
-		else if (input == "a3")
-		{
-			gameboard[3][1] = 'O';
-			turn = false;
-		}
-		else if (input == "b1")
-		{
-			gameboard[1][2] = 'O';
-			turn = false;
-		}
-		else if (input == "b2")
-		{
-			gameboard[2][2] = 'O';
-			turn = false;
-		}
-		else if (input == "b3")
-		{
-			gameboard[3][2] = 'O';
-			turn = false;
-		}
-		else if (input == "c1")
-		{
-			gameboard[1][3] = 'O';
-			turn = false;
-		}
-		else if (input == "c2")
-		{
-			gameboard[2][3] = 'O';
-			turn = false;
-		}
-		else if (input == "c3")
-		{
-			gameboard[3][3] = 'O';
-			turn = false;
-		}
-		else
-		{
-			std::cout << "That is not a valid input... try again\n";
-		}
+		// Column 3 (c)
+		{"c1", {1, 3}}, // Row 1, Column 3
+		{"c2", {2, 3}}, // Row 2, Column 3
+		{"c3", {3, 3}}  // Row 3, Column 3
+	};
 
-
+	if (stringsToRowsAndColumns.find(input) == stringsToRowsAndColumns.end())
+	{
+		std::cout << "The input " << input << "is not an allowed position\n";
+		__debugbreak(); 
 	}
+
+	return stringsToRowsAndColumns[input];
 }
 
-void player2Turn(char gameboard[4][4])
+void playerTurn(std::vector<std::vector<char>>& gameboard, const std::string& pid)
 {
 	bool turn = true;
 	std::string input{};
 	std::getline(std::cin, input);
 	while (turn)
 	{
-		if (input == "a1")
-		{
-			if (playedPositions.find({ 1, 1 }) == playedPositions.end())
-			{
-				gameboard[1][1] = 'O';
-				playedPositions.insert({ 1, 1 });
-				turn = false;
-			}
+		auto pair = convertStringInputToRowAndColumn(input); 
 
-			else
-			{
-				std::cout << "That spot is already taken\n";
-				std::getline(std::cin, input);
-			}
-		}
-		else if (input == "a2")
+
+
+		if (pid == "P1")
 		{
-			gameboard[2][1] = 'X';
-			turn = false;
+			gameboard[pair.first][pair.second] = 'X'; 
 		}
-		else if (input == "a3")
+
+		else if (pid == "P2")
 		{
-			gameboard[3][1] = 'X';
-			turn = false;
-		}
-		else if (input == "b1")
-		{
-			gameboard[1][2] = 'X';
-			turn = false;
-		}
-		else if (input == "b2")
-		{
-			gameboard[2][2] = 'X';
-			turn = false;
-		}
-		else if (input == "b3")
-		{
-			gameboard[3][2] = 'X';
-			turn = false;
-		}
-		else if (input == "c1")
-		{
-			gameboard[1][3] = 'X';
-			turn = false;
-		}
-		else if (input == "c2")
-		{
-			gameboard[2][3] = 'X';
-			turn = false;
-		}
-		else if (input == "c3")
-		{
-			gameboard[3][3] = 'X';
-			turn = false;
-		}
-		else
-		{
-			std::cout << "That is not a valid input... try again\n";
+			gameboard[pair.first][pair.second] = 'O';
 		}
 	}
 }
